a5ee9648d0f59baf9589658a5231f0cd8943a805
Tatsuki <iputatsuki@gmail.com>
2025-03-23 02:47:32 +0000

    DJANGO SECRET KEY修正

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ MEDIA_ROOT = os.path.join(MEDIA_BASE_DIR, "media")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@s$@-ls((6ecdisymlh84wg%4n8e(wuu(*=4+)ieed_cjemn!7"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
